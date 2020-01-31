640ec2be6561772a5e8c72d9bf4d980a0671f71f
Max Holliday <maholli@stanford.edu>
2020-01-30 17:16:17 -0800

    added ability to control GATE pin (on/off)

--- adm1176.py
+++ adm1176.py
@@ -32,33 +32,44 @@ class ADM1176:
 
         self.config('V_CONT,I_CONT')
     _cmd=bytearray(1)
-    _cmd[0] = 0b0000000
+    _cmd[0] = 0x00
+    _extcmd=bytearray(2)
+    _extcmd[0] = 0x83
 
     _BUFFER = bytearray(3)
     _STATUS = bytearray(1)
     def config(self, value):
         if 'V_CONT' in value:
-            print('vcont')
             self._cmd[0] |= (1<<0)
         if 'V_ONCE' in value:
             self._cmd[0] |= (1<<1)
         if 'I_CONT' in value:
-            print('icont')
             self._cmd[0] |= (1<<2)
         if 'I_ONCE' in value:
             self._cmd[0] |= (1<<3)
         if 'VRANGE' in value:
             self._cmd[0] |= (1<<4)
         with self.i2c_device as i2c:
-            i2c.write(self._cmd, end=1, stop=True)
+            i2c.write(self._cmd)
 
     def read(self):
         with self.i2c_device as i2c:
             i2c.readinto(self._BUFFER)
         raw_voltage = ((self._BUFFER[0]<<8) | (self._BUFFER[2]&DATA_V_MASK))>>4
         raw_current = (self._BUFFER[1]<<8) | (self._BUFFER[2]&DATA_I_MASK)
-        _voltage = (26.35/4096)*raw_voltage
-        _current = ((0.10584/4096)*raw_current)/self.sense_resistor
+        _voltage = (26.35/4096)*raw_voltage # volts
+        _current = ((0.10584/4096)*raw_current)/self.sense_resistor # amperes
         return (_voltage,_current)
 
+    @property
+    def OFF(self):
+        self._extcmd[1]=1
+        with self.i2c_device as i2c:
+            i2c.write(self._extcmd)
 
+    @property
+    def ON(self):
+        self._extcmd[1]=0
+        with self.i2c_device as i2c:
+            i2c.write(self._extcmd)
+        self.config('V_CONT,I_CONT')
